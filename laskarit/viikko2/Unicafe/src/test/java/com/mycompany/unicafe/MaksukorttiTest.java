47d409db31d973b3bef14f586ee10b0abc6d9864
Elias Herranen <elias.herranen@gmail.com>
2020-11-02 12:59:59 +0200

    laskariviikko2 tehtavat tehty

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -1,5 +1,6 @@
 package com.mycompany.unicafe;
 
+import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,11 +11,39 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(1000);
+    }
+    
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+    
+    @Test
+    public void kortinSaldoLisayksenJalkeenOikein() {
+        kortti.lataaRahaa(10);
+        assertEquals("saldo: 10.10", kortti.toString());
+
+    }
+    
+    @Test
+    public void saldoVaheneeOikein() {
+        kortti.otaRahaa(500);
+        assertEquals("saldo: 5.0", kortti.toString());
+        kortti.otaRahaa(600);
+        assertEquals("saldo: 5.0", kortti.toString());
+        assertEquals(false, kortti.otaRahaa(600));
+        assertEquals(true, kortti.otaRahaa(500));
+
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    
+    @Test
+    public void saldoOikein() {
+        assertEquals(1000, kortti.saldo());
     }
 }
